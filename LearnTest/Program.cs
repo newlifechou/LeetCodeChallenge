7e2da51dd6618b9c552ea49c7c0c98406ccbd829
xs.zhou <newlifechou@outlook.com>
2017-05-07 22:24:57 +0800

    create a console project to test algorithm stuffs

---
+++ LearnTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i=i*2)
+            {
+                System.Diagnostics.Debug.Print(i.ToString());
+                for (int j = 0; j < 1000; j++)
+                {
+                    sum++;
+
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.Read();
+
+        }
+    }
+}
