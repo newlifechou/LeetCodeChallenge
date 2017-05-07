f98bc8227d488b1295584749766d9993589f83e5
xs.zhou <newlifechou@outlook.com>
2017-05-07 20:35:36 +0800

    binary search ok

--- Algorithm/AlgorithmSolution.cs
+++ Algorithm/AlgorithmSolution.cs
@@ -8,5 +8,30 @@ namespace Algorithm
 {
     public class AlgorithmSolution
     {
+        public int BinarySearch(int key, int[] arrays)
+        {
+            int low = 0;
+            int high = arrays.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (key == arrays[mid])
+                {
+                    return mid;
+                }
+                else if (key > arrays[mid])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+
+            }
+
+            return -1;
+
+        }
     }
 }
---
+++ AlgorithmTests/AlgorithmSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class AlgorithmSolutionTests
+    {
+        [TestMethod()]
+        public void BinarySearchTest()
+        {
+            int[] arrays = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int key = 8;
+            int expect = 7;
+            int reality = new AlgorithmSolution().BinarySearch(key, arrays);
+            Assert.AreEqual(expect, reality);
+        }
+    }
+}
