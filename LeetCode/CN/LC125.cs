6336e221a087c30588988449bd9d5826e9c6796b
xszhou <newlifechou@outlook.com>
2022-07-23 19:20:20 +0800

    change

---
+++ LeetCode/CN/LC125.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN
+{
+    internal class LC125
+    {
+        public bool IsPalindrome(string s)
+        {
+            int n = s.Length;
+
+            int left = 0, right = n - 1;
+            while (left<right)
+            {
+                while (left < right && !char.IsLetterOrDigit(s[left]))
+                {
+                    left++;
+                }
+                while (left < right && !char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                    {
+                        return false;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+
+            return true;
+        }
+    }
+}
