5cb14a1c8ac7cacd6594ca272316d9ad44e7f146
xs.zhou <xs.zhou@outlook.com>
2022-07-25 09:29:21 +0800

    done

---
+++ LeetCode/CN/LC033.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN
+{
+    internal class LC033
+    {
+        /// <summary>
+        /// 将数组一分为二，其中一定有一个是有序的，另一个可能是有序，也能是部分有序。
+        /// 此时有序部分用二分法查找。无序部分再一分为二，其中一个一定有序，另一个可能有序，可能无序。就这样循环.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length == 0)
+                return -1;
+            if (nums.Length == 1)
+                return nums[0] == target ? 0 : -1;
+
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+
+                if (nums[0] <= nums[mid])
+                {
+                    if (nums[0] <= target && target < nums[mid])
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                else
+                {
+                    //判断中间值和target大小且target和尾值的大小
+                    if (nums[mid] < target && target <= nums[nums.Length - 1])
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/CN/LC037.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN
+{
+    internal class LC037
+    {
+        public void SolveSudoku(char[][] board)
+        {
+            //init
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i][j] == '.')
+                    {
+                        //存储spaces的位置
+                        spaces.Add(new int[] { i, j });
+                    }
+                    else
+                    {
+                        int digit = board[i][j] - '0' - 1;
+                        line[i, digit] = colomn[j, digit] = block[i / 3, j / 3, digit] = true;
+                    }
+                }
+            }
+
+
+            dfs(board, 0);
+        }
+
+        //表示第i行，第j列，第x,y个九宫格填写每个数字的情况
+        //line[1,8]=第1行，数字8是否满足要求
+        private bool[,] line = new bool[9, 9];
+        private bool[,] colomn = new bool[9, 9];
+        private bool[,,] block = new bool[3, 3, 9];
+
+        //dfs有效标志
+        private bool valid = false;
+        //存储空白字符，等待递归回溯
+        private List<int[]> spaces = new List<int[]>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="pos"></param>
+        private void dfs(char[][] board, int pos)
+        {
+            //
+            if (pos == spaces.Count)
+            {
+                valid = true;
+                return;
+            }
+
+            int[] space = spaces[pos];
+            //获取当前space的i,j
+            int i = space[0], j = space[1];
+
+            for (int digit = 0; digit < 9 && !valid; digit++)
+            {
+                if (!line[i, digit] && !colomn[j, digit] && !block[i / 3, j / 3, digit])
+                {
+                    line[i, digit] = colomn[j, digit] = block[i / 3, j / 3, digit] = true;
+                    board[i][j] = (char)(digit + '0' + 1);
+                    
+                    dfs(board, pos + 1);
+                    //恢复
+                    line[i, digit] = colomn[j, digit] = block[i/3, j/3, digit] = false;
+                }
+            }
+
+
+        }
+
+
+
+    }
+}
