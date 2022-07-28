baa1c96de0724d8dacf92bf411ddabcb04f623b3
xs.zhou <xs.zhou@outlook.com>
2022-07-29 00:10:23 +0800

    add JZ

---
+++ LeetCode/JZ/JZ009.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.JZ
+{
+    internal class JZ009
+    {
+    }
+
+    public class CQueue
+    {
+        private Stack<int> stackIn;
+        private Stack<int> stackOut;
+        public CQueue()
+        {
+            stackIn = new Stack<int>();
+            stackOut = new Stack<int>();
+        }
+
+        public void AppendTail(int value)
+        {
+            stackIn.Push(value);
+        }
+
+        public int DeleteHead()
+        {
+            if (stackIn.Count == 0 && stackOut.Count == 0)
+            {
+                return -1;
+            }
+
+            if (stackOut.Count == 0)
+            {
+                //此时转移数据从in到out
+                while (stackIn.Count > 0)
+                {
+                    stackOut.Push(stackIn.Pop());
+                }
+            }
+
+            return stackOut.Pop();
+        }
+    }
+
+
+}
---
+++ LeetCode/JZ/JZ040.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.JZ
+{
+    internal class JZ040
+    {
+        public int[] GetLeastNumbers(int[] arr, int k)
+        {
+            int[] ans=new int[k];
+            Array.Sort(arr);
+            for (int i = 0; i < k; i++)
+            {
+                ans[i] = arr[i];
+            }
+            return ans;
+        }
+    }
+}
