8b3d5d4f9dd7eeec872ee58ea0bf1e59762fcc3d
xs.zhou <xs.zhou@outlook.com>
2022-08-01 06:49:24 +0800

    done

---
+++ LeetCode/CN/LC171.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN
+{
+    public class LC171
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int ans = 0;
+            int ratio = 1;
+            foreach (var item in columnTitle.Reverse())
+            {
+                int tailNumber = item - 'A' + 1;
+                ans += tailNumber * ratio;
+                ratio *= 26;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/CN/LC171Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.CN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN.Tests
+{
+    [TestClass()]
+    public class LC171Tests
+    {
+        [TestMethod()]
+        public void TitleToNumberTest()
+        {
+
+            int ans = new LC171().TitleToNumber("ZY");
+            Assert.AreEqual(701,ans);
+        }
+    }
+}
