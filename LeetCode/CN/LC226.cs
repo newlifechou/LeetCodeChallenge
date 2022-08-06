4bbb7f34dc5a6820e43d26886477e21928744d10
xs.zhou <xs.zhou@outlook.com>
2022-08-06 13:20:37 +0800

    done

---
+++ LeetCode/CN/LC226.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN
+{
+    /// <summary>
+    /// 翻转二叉树
+    /// 左右互换
+    /// </summary>
+    internal class LC226
+    {
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+            TreeNode left = InvertTree(root.left);
+            TreeNode right = InvertTree(root.right);
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+
+
+    }
+}
---
+++ LeetCode/CN/LC228.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN
+{
+    internal class LC228
+    {
+        public IList<string> SummaryRanges(int[] nums)
+        {
+            List<string> ranges = new List<string>();
+            int i = 0;
+            int n = nums.Length;
+            while (i < n)
+            {
+                //找出low和high
+                int low = i;
+                i++;
+                while (i < n && nums[i] - nums[i - 1] == 1)
+                {
+                    i++;
+                }
+                int high = i-1;
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append(nums[low]);
+                if (low < high)
+                {
+                    sb.Append("->");
+                    sb.Append(nums[high]);
+                }
+                ranges.Add(sb.ToString());
+            }
+            return ranges;
+        }
+    }
+}
