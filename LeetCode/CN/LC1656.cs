6db7bcf3411e0029b0c072267148e50e3866843a
xs.zhou <xs.zhou@outlook.com>
2022-08-16 09:32:38 +0800

    done

---
+++ LeetCode/CN/LC1656.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN
+{
+    internal class LC1656
+    {
+    }
+    public class OrderedStream
+    {
+        private string[] stream;
+        private int ptr;
+        public OrderedStream(int n)
+        {
+            stream = new string[n + 1];
+            ptr = 1;
+        }
+
+        public IList<string> Insert(int idKey, string value)
+        {
+            stream[idKey] = value;
+            IList<string> res = new List<string>();
+            while (ptr < stream.Length && stream[ptr] != null)
+            {
+                res.Add(stream[ptr]);
+                ptr++;
+            }
+            return res;
+        }
+    }
+
+
+}
---
+++ LeetCode/CN/LC345.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CN
+{
+    internal class LC345
+    {
+        public string ReverseVowels(string s)
+        {
+            int n = s.Length;
+            char[] arr = s.ToCharArray();
+            int i = 0, j = n - 1;
+            while (i < j)
+            {
+                while (i < n && "aeiouAEIOU".IndexOf(arr[i]) < 0)
+                {
+                    i++;
+                }
+                while (j > 0 && "aeiouAEIOU".IndexOf(arr[j]) < 0)
+                {
+                    j--;
+                }
+                if (i < j)
+                {
+                    char temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            return new string(arr);
+        }
+
+
+    }
+}
